614de36cd60cbba7a77fe0572a35f9a000f5d1c0
Yash Thakre <86224563+yash9904@users.noreply.github.com>
2024-08-25 20:48:36 +0530

    add streaming response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,19 @@
+import os
+
 import requests
 import streamlit as st
 from country_list import countries_for_language
+from langchain_ibm import WatsonxLLM
 
+os.environ["WATSONX_APIKEY"] = st.secrets["api"]["key"]
 # IBM IAM Token Endpoint and Watsonx.ai API details
 IAM_TOKEN_URL = "https://iam.cloud.ibm.com/identity/token"
 API_URL = "https://us-south.ml.cloud.ibm.com/ml/v1/text/generation?version=2023-05-29"
 
 
-# Obtain IAM token using API Key
-def get_iam_token(api_key):
-    response = requests.post(
-        IAM_TOKEN_URL,
-        headers={"Content-Type": "application/x-www-form-urlencoded"},
-        data={
-            "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
-            "apikey": api_key,
-        },
-    )
-    if response.status_code != 200:
-        st.error("Error obtaining IAM token")
-        return None
-    return response.json().get("access_token")
-
-
 # Get tax strategy from IBM Watsonx.ai API
-def get_tax_strategy(data, iam_token):
-    body = {
-        "input": f"""
+def generate_tax_strategy(data):
+    prompt = f"""
             Generate a personalized tax-saving strategy for a user based on their financial profile. 
             Assume the numbers to be in Local Currency.
             Input:  
@@ -36,38 +23,27 @@ def get_tax_strategy(data, iam_token):
                 Investment: {data['investment']}
                 Deductions: {data['deductions']}
 
-            Respond with a structured tax-saving strategy for the user. The text output should contain the country currency, the calculation between before & after -tax income, it also should be well formatted and easy to understand.
-        """,
-        "parameters": {
+            Respond with a structured tax-saving strategy for the user. 
+            The text output should contain the calculation between before & after-tax income.
+            It also should be well formatted (numbered/bulleted/paragraphs/formulae) and easy to understand.
+            Make sure to use currency symbols (Example $, €, etc) and percentages where necessary.
+        """
+
+    watsonx_llm = WatsonxLLM(
+        model_id="ibm/granite-13b-chat-v2",
+        project_id=st.secrets["project"]["id"],
+        url="https://us-south.ml.cloud.ibm.com",
+        params={
             "decoding_method": "greedy",
             "max_new_tokens": 1000,
             "repetition_penalty": 1,
         },
-        "model_id": "ibm/granite-13b-chat-v2",
-        "project_id": st.secrets["project"]["id"],
-    }
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {iam_token}",
-    }
-    response = requests.post(API_URL, headers=headers, json=body)
-    if response.status_code != 200:
-        st.error("Error fetching data from the API")
-        return None
-
-    response_json = response.json()
-    results = response_json.get("results", [])
-    if results:
-        return results[0].get("generated_text", "No strategy found")
-    return "No strategy found"
+    )
+    st.write_stream(watsonx_llm.stream(prompt))
 
 
 def display_form():
     st.title("🧾 Tax Optimization Strategy 🏦")
-
-    # Read API Key from Streamlit secrets
-    api_key = st.secrets["api"]["key"]
 
     st.sidebar.header("User Information")
 
@@ -102,7 +78,14 @@ def display_form():
     if deductions is None:
         st.sidebar.error("Please enter your deductions.")
 
-    if all([earnings is not None, tax is not None, investment is not None, deductions is not None]):
+    if all(
+        [
+            earnings is not None,
+            tax is not None,
+            investment is not None,
+            deductions is not None,
+        ]
+    ):
         st.write("All required inputs are provided.")
 
     additional_info = st.sidebar.text_area("Additional Information")
@@ -116,12 +99,10 @@ def display_form():
             "deductions": deductions,
             "additional_info": additional_info,
         }
-        iam_token = get_iam_token(api_key)
-        if iam_token:
-            strategy = get_tax_strategy(data, iam_token)
-            if strategy:
-                st.write("**Personalized Tax-Saving Strategy:**")
-                st.write(strategy)
+        try:
+            generate_tax_strategy(data)
+        except Exception as e:
+            st.error(f"Error: {e}")
 
 
 if __name__ == "__main__":
